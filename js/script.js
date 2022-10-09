243094caf54b29745e390d5f89281908a7277c9e
Olena Travnikova <elena@travnikova.pro>
2022-10-09 20:11:49 +0200

    timer edition, in case if deadline was ended

--- js/script.js
+++ js/script.js
@@ -38,16 +38,25 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    
-    //Timer
-    const deadline = '2022-10-25';
 
-    function getTimeRemaining(endtime) {
-        let t = Date.parse(endtime) - Date.parse(new Date());
-        let days = Math.floor(t / (1000 * 60 * 60 * 24));
-        let hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-        let minutes = Math.floor(t / (1000 * 60) % 60);
-        let seconds = Math.floor((t / 1000) % 60);
+    //Timer
+    const deadline = '2022-11-28';
+
+    function getTimeRemaining(endTime) {
+        let t = Date.parse(endTime) - Date.parse(new Date());
+        let days, hours, minutes, seconds;
+
+        if (t <= 0) {
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0
+        } else {
+            days = Math.floor(t / (1000 * 60 * 60 * 24));
+            hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+            minutes = Math.floor(t / (1000 * 60) % 60);
+            seconds = Math.floor((t / 1000) % 60);
+        }
         return {
             'total': t,
             'days': days,
@@ -57,7 +66,7 @@ window.addEventListener('DOMContentLoaded', () => {
         };
     }
 
-    
+
     function getZero(num) {
         if (num >= 0 && num < 10) {
             return `0${num}`
@@ -65,8 +74,8 @@ window.addEventListener('DOMContentLoaded', () => {
             return num
         }
     }
-    
-    function setClock(selector, endtime) {
+
+    function setClock(selector, endTime) {
         const timer = document.querySelector(selector);
         const days = timer.querySelector('#days');
         const hours = timer.querySelector('#hours');
@@ -75,10 +84,9 @@ window.addEventListener('DOMContentLoaded', () => {
         const timeInterval = setInterval(updateClock, 1000);
 
         updateClock();
-        
-        function updateClock() {
-            const t = getTimeRemaining(endtime);
 
+        function updateClock() {
+            const t = getTimeRemaining(endTime);
             days.innerHTML = getZero(t.days);
             hours.innerHTML = getZero(t.hours);
             minutes.innerHTML = getZero(t.minutes);
