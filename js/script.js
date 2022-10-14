aa1c7a4a285c7106a66c3a27234e515d70b8d628
Olena Travnikova <elena@travnikova.pro>
2022-10-14 13:57:30 +0200

    cards with js classes - my personal code

--- js/script.js
+++ js/script.js
@@ -165,65 +165,60 @@ function copyOff() {
 }
 
 //classes for cards
-class MenuCard {
-    constructor(src, alt, title, descr, price, parentSelector) {
+class Card {
+    constructor(src, alt, title, desc, price, parentSelector) {
         this.src = src;
         this.alt = alt;
         this.title = title;
-        this.descr = descr;
+        this.desc = desc;
         this.price = price;
         this.parent = document.querySelector(parentSelector);
-        this.transfer = 40;
-        this.changeToUAH()
-
+        this.course = 40;
+        this.fromUSDToUAH();
     }
-    changeToUAH() {
-       this.price = +this.price * this.transfer
-    }
+    fromUSDToUAH() {
+         this.price =  +this.price * this.course
+}
     render() {
-        const element = document.createElement('div');
-        element.innerHTML = `
-                <div class="menu__item">
+        const newDiv = document.createElement('div');
+        newDiv.innerHTML = `
+        <div class="menu__item">
                     <img src=${this.src} alt=${this.alt}>
                     <h3 class="menu__item-subtitle">${this.title}</h3>
-                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-descr">${this.desc}</div>
                     <div class="menu__item-divider"></div>
                     <div class="menu__item-price">
                         <div class="menu__item-cost">Цена:</div>
                         <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-                        </div>
-        `;
-        this.parent.append(element);
+                    </div>
+        `
+        this.parent.append(newDiv);
     }
 }
-new MenuCard(
+
+new Card(
     "img/tabs/vegy.jpg",
     "vegy",
-    "Меню Фитнес",
-    "Меню \"Фитнес\" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это новый продукт с оптимальной ценой и высоким качеством!",
-    16,
-    ".menu .container"
+    'Меню "Фитнес"',
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    14,
+    '.menu .container'
 ).render();
 
-new MenuCard(
-    "img/tabs/elite.jpg",
-    "elite",
-    "Меню Премиум",
-    "В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
-    21,
-    ".menu .container"
-).render();
-
-
-new MenuCard(
+new Card(
     "img/tabs/post.jpg",
     "post",
-    "Меню Постное",
-    "Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
-    18,
-    ".menu .container"
+    'Меню "Постное"',
+    'Меню "Постное" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    16,
+    '.menu .container'
 ).render();
 
-
-
-
+new Card(
+    "img/tabs/elite.jpg",
+    "elite",
+    'Меню "Премиум"',
+    'Меню "Премиум" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    19,
+    '.menu .container'
+).render();
