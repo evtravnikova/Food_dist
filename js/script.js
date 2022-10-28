bdc75fc4735d600bae927d25671ccac02c3d8a0e
Olena Travnikova <elena@travnikova.pro>
2022-10-28 17:13:04 +0200

    Slider ed.

--- js/script.js
+++ js/script.js
@@ -342,10 +342,10 @@ function changeSlideImg() {
 
 
     next.addEventListener('click', () => {
-        if (offset === (+width.slice(0, width.length - 2) * (slides.length - 1))) {
+        if (offset === (deleteNotDigits(width) * (slides.length - 1))) {
             offset = 0;
         } else {
-            offset += +width.slice(0, width.length - 2);
+            offset += deleteNotDigits(width);
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -366,9 +366,9 @@ function changeSlideImg() {
 
     prev.addEventListener('click', () => {
         if (offset === 0) {
-            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+            offset = deleteNotDigits(width) * (slides.length - 1);
         } else {
-            offset -= +width.slice(0, width.length - 2);
+            offset -= deleteNotDigits(width);
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -392,7 +392,7 @@ function changeSlideImg() {
             const slideTo = +e.target.getAttribute('data-slide-to');
 
             slideIndex = slideTo;
-            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+            offset = deleteNotDigits(width) * (slideTo - 1);
 
             slidesField.style.transform = `translateX(-${offset}px)`;
 
@@ -408,6 +408,10 @@ function changeSlideImg() {
     function dotsOpacity() {
         dots.forEach(dot => dot.style.opacity = '.5');
         dots[slideIndex - 1].style.opacity = 1;
+    }
+    
+    function deleteNotDigits(str) {	
+        return +str.replace(/\D/g, '');	
     }
 }
 
