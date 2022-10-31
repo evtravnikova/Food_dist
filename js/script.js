197f311c7ff5f0a089608f82978be4634fe3ac84
Olena Travnikova <elena@travnikova.pro>
2022-10-31 21:23:07 +0100

    Calories calculator

--- js/script.js
+++ js/script.js
@@ -303,13 +303,17 @@ function changeSlideImg() {
         width = window.getComputedStyle(slidesWrapper).width,
         slidesField = document.querySelector('.offer__slider-inner');
 
-    if (slideIndex < 10) {
+    if (slideIndex < 10 && slides.length < 10) {
+        total.textContent = `/0${slides.length}`;
+        current.textContent = `0${slideIndex}`;
+    }
+    if (slideIndex < 10 && slides.length >= 10) {
         total.textContent = `/${slides.length}`;
         current.textContent = `0${slideIndex}`;
-    } else {
-        total.textContent = '/' + slides.length;
+    } /*else {
+        total.textContent = slides.length;
         current.textContent = slideIndex;
-    }
+    }*/
 
     slidesField.style.width = 100 * slides.length + '%';
     slidesField.style.display = 'flex';
@@ -409,9 +413,78 @@ function changeSlideImg() {
         dots.forEach(dot => dot.style.opacity = '.5');
         dots[slideIndex - 1].style.opacity = 1;
     }
-    
-    function deleteNotDigits(str) {	
-        return +str.replace(/\D/g, '');	
+
+    function deleteNotDigits(str) {
+        return +str.replace(/\D/g, '');
     }
+
+
+    //Calories calculator
+    const result = document.querySelector('.calculating__result span');
+    let sex = 'female',
+        height,
+        weight,
+        age,
+        ratio = 1.375;
+
+    function calcTotal() {
+        if (!sex || !height || !weight || !age || !ratio) {
+            result.textContent = '...';
+            return;
+        }
+        if (sex === 'female') {
+            result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio).toString();
+        } else {
+            result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio).toString();
+        }
+    }
+
+    calcTotal();
+
+    function getStaticData(parentSelector, activeClass) {
+        const elements = document.querySelectorAll(`${parentSelector} div`);
+        
+        elements.forEach(elem => {
+            elem.addEventListener('click', (e) => {
+            if (e.target.getAttribute('data-ratio')) {
+                ratio = +e.target.getAttribute('data-ratio');
+            } else {
+                sex = e.target.getAttribute('id');
+            }
+
+            elements.forEach(elem => {
+                elem.classList.remove(activeClass);
+            });
+            e.target.classList.add(activeClass);
+            
+            calcTotal();
+        })
+        })
+    }
+
+    getStaticData('#gender', 'calculating__choose-item_active');
+    getStaticData('.calculating__choose_big', 'calculating__choose-item_active');
+
+    function getDynamicData(selector) {
+        const input = document.querySelector(selector);
+        input.addEventListener('input', () => {
+            switch (input.getAttribute('id')) {
+                case 'height':
+                    height = +input.value;
+                    break;
+                case 'weight':
+                    weight = +input.value;
+                    break;
+                case 'age':
+                    age = +input.value;
+                    break;
+            }
+            calcTotal();
+        });
+    }
+
+    getDynamicData('#height');
+    getDynamicData('#weight');
+    getDynamicData('#age');
 }
 
